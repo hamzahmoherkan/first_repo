bb1c8a563f3e287c2dada1b5321fd9cd99c9ee65
hamzahmoherkan <cosmohacker22@gmail.com>
2024-09-30 12:35:15 +0530

    pythoncode for webscraping

---
+++ webscraping.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+URL = "http://www.example.com"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+
+#scrpy
+import scrapy
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ['http://quotes.toscrape.com/tag/humor/',]
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {'quote': quote.css('span.text::text').get()}
+
+
+
+#selenium
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.get("http://www.example.com")
